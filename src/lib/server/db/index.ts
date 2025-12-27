1aaee9558c3bf27b6e4a19cafd88c29e5d081bcd
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-12-27 15:00:35 +0700

    fix: env database

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,8 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(env.POSTGRES_URL);
 
 export const db = drizzle(client, { schema });
